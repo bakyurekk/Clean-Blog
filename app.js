4b088564495186c74d3bf8f2fabfcd91f63f35c8
burkanakyurek <burkanakyurek@hotmail.com>
2021-05-28 22:55:54 +0300

    Template Engine

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 const app = express();
 
 PORT = 3000;
 
-app.get('/', (req, res) => {
-    const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
-    res.status(200).json(blog);
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// ROUTERSS
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/add-post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(PORT, () => {
-    console.log(`Listining port ${PORT}`)
+  console.log(`Listining port ${PORT}`);
 });
