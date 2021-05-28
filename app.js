41e14e8ee0888a5d46ae5529addab91c0ca51b54
burkanakyurek <burkanakyurek@hotmail.com>
2021-05-28 15:44:07 +0300

    First Comment

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require('express');
 const app = express();
 
 PORT = 3000;
+
+app.get('/', (req, res) => {
+    const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+
+    res.status(200).json(blog);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listining port ${PORT}`)
+});
