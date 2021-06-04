dced3d522b7872636ce3b78ca301618aa3c3dabf
burkanakyurek <burkanakyurek@hotmail.com>
2021-06-05 00:30:06 +0300

    Added Page Title

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,20 +1,27 @@
 const Post = require('../models/Post');
 
 exports.getAboutPage = (req, res) => {
-  res.render('about');
+  res.render('about', {
+    pageTitle: 'About',
+  });
 };
 
 exports.getPostPage = (req, res) => {
-  res.render('post');
+  res.render('post', {
+    pageTitle: 'Post',
+  });
 };
 
 exports.getAddPostPage = (req, res) => {
-  res.render('add_post');
+  res.render('add_post', {
+    pageTitle: 'Add Post',
+  });
 };
 
 exports.getEditPage = async (req, res) => {
   const post = await Post.findOne({ _id: req.params.id });
   res.render('edit', {
+    pageTitle: 'Edit Post',
     post,
   });
 };
